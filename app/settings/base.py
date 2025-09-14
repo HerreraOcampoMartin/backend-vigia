cc1dfcbb191dd1db11a55c6ae5e65051ee7980de
Martín Herrera Ocampo <herreraocampomartin@gmail.com>
2025-09-14 17:22:44 -0300

    Eliminar un TODO

--- app/settings/base.py
+++ app/settings/base.py
@@ -81,7 +81,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'app.wsgi.application'
 
-# SECURITY WARNING: keep the secret key used in production secret! TODO
+# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # Password validation
