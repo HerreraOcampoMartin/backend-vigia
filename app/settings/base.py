90c7a6ed9ce07fe82090f93b4802713472f0232b
Martín Herrera Ocampo <herreraocampomartin@gmail.com>
2025-09-15 10:36:49 -0300

    Desactivar el renderer de html desde el navegador

--- app/settings/base.py
+++ app/settings/base.py
@@ -134,7 +134,10 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 # Custom User model
